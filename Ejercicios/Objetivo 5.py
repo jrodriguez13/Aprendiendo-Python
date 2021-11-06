5648802a19cdbda9a45a72b2ad963649ce380e85
Javier Rodriguez <63521517+jrodriguez13@users.noreply.github.com>
2021-11-06 16:45:06 -0300

    Add files via upload

---
+++ Ejercicios/Objetivo 5.py
@@ -0,0 +1,45 @@
+# EJERCICIO 1
+
+# def welcome():
+#     print('¡Hola amigo!')
+#     return
+
+# welcome()
+# welcome()
+# welcome()
+
+# EJERCICIO 2
+
+# def welcome(nombre):
+#     print('¡Hola ' + nombre + '!')
+#     return
+
+# welcome('Javier')
+# welcome('Maria')
+
+# # EJERCICIO 3
+
+# def factorial(n):
+#     tmp = 1
+#     for i in range(n):
+#         tmp *= i+1
+#     return tmp
+
+# print(factorial(4))
+# print(factorial(20))
+
+# # EJERCICIO 4
+
+# def impuesto(monto, tasa=21):
+#     return monto + monto*tasa/100
+
+# print(impuesto(1000,10))
+# print(impuesto(1000))
+
+# # EJERCICIO 5
+
+# def promedio(*lista):
+#     return sum(lista)/len(lista)
+
+# print(promedio(1, 2, 3, 4, 5))
+# print(promedio(2.3, 5.7, 6.8, 9.7, 12.1, 15.6))
